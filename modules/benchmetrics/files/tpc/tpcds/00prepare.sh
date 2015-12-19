fe89e51dbe2aecb95af1733f8913e49a423a6564
Carter Shanklin <carter@HW11972.local>
2015-12-18 19:58:21 -0800

    Adding TPC DS

---
+++ modules/benchmetrics/files/tpc/tpcds/00prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+SCALE=2
+
+# Don't do anything if the data is already loaded.
+hdfs dfs -ls /apps/hive/warehouse/tpcds_bin_partitioned_orc_$SCALE.db >/dev/null
+
+if [ $? -ne 0 ];  then
+	# Build it.
+	echo "Building the data generator"
+	cd /vagrant/modules/benchmetrics/files/tpc/tpcds
+	sh /vagrant/modules/benchmetrics/files/tpc/tpcds/tpcds-build.sh
+
+	# Generate and optimize the data.
+	echo "Generate the data at scale $SCALE"
+	sh /vagrant/modules/benchmetrics/files/tpc/tpcds/tpcds-setup.sh $SCALE
+fi
