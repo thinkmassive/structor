7d8a4f3b1c298f156b70d8c591948adada334406
Carter Shanklin <carter@HW11972.local>
2015-12-21 15:39:49 -0800

    Be much more aggressive cleaning up data before and after runs.

--- modules/benchmetrics/files/etl/all_types/00prepare.sh
+++ modules/benchmetrics/files/etl/all_types/00prepare.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+sudo usermod -a -G hadoop vagrant
+
 sudo service hive-server2 stop
+hdfs dfs -rmr /user/vagrant/.Trash
+sudo bash -c "rm -rf /var/lib/hadoop/yarn/node/usercache/vagrant/appcache/application_*"
 sudo pip install importlib
 
 # Generate data.
@@ -12,3 +16,4 @@ done
 wait
 
 hive -f /vagrant/modules/benchmetrics/files/etl/all_types/load_all_types_csv.sql
+rm -f /tmp/all_types*
