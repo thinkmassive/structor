34c9d04b5b0f90594d3d4f0052b99724f05dd863
Carter Shanklin <carter@HW11972.local>
2015-12-30 10:32:07 -0800

    Only mark as done upon successful exit.

--- modules/benchmetrics/files/explain/sa_orig_nocbo/00prepare.sh
+++ modules/benchmetrics/files/explain/sa_orig_nocbo/00prepare.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-( cd /vagrant/modules/sa_golden/files ; /vagrant/modules/sa_golden/files/load_hive.sh )
+SUCCESS=/home/vagrant/sa_golden_stats_successful
+
+if [ ! -e $SUCCESS ]; then
+	( cd /vagrant/modules/sa_golden/files ; /vagrant/modules/sa_golden/files/load_schema_only.sh && touch $SUCCESS )
+fi
