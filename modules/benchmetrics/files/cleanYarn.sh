b370cf9de63b76910620c4adb7b449a98fd6b455
Carter Shanklin <carter@HW11972.local>
2016-03-06 08:20:18 -0800

    Also clean out hbase's directory (for bulkloads)

--- modules/benchmetrics/files/cleanYarn.sh
+++ modules/benchmetrics/files/cleanYarn.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 sudo bash -c "find /var/lib/hadoop/yarn/node/usercache/vagrant/appcache -cmin +5 -delete"
+sudo bash -c "find /var/lib/hadoop/yarn/node/usercache/hbase/appcache -cmin +5 -delete"
 true
