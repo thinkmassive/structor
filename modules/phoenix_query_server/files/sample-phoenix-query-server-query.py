6af37201a3954cc141f78d92566527c1138d442c
Carter Shanklin <carter@HW11972.local>
2016-05-08 09:15:02 -0700

    Kerberos support plus setup instructions

--- modules/phoenix_query_server/files/sample-phoenix-query-server-query.py
+++ modules/phoenix_query_server/files/sample-phoenix-query-server-query.py
@@ -3,19 +3,37 @@
 import json
 import pprint
 import random
+import socket
 import sys
-import urllib2
+import urllib2 as url
+
+# To get this example on secure:
+#  Be on CentOS 7. Ubuntu 14 might work too.
+#  Switch PQS to JSON mode.
+#  sudo yum install -y epel-release
+#  sudo yum install -y python-pip krb5-devel python-devel
+#  sudo pip install urllib2_kerberos
+#  sudo pip install kerberos
+#  Change secure to True below
+
+secure = False
+if secure:
+    import urllib2_kerberos as u2k
 
 # Sample query counts the number of items in the system catalog.
-pqsUrl = "http://localhost:8765/"
+pqsUrl = "http://{0}:8765/".format(socket.gethostname())
 pp = pprint.PrettyPrinter(indent=2, stream=sys.stdout)
 
 def make_request(pqsUrl, request):
 	print "Sending : "
 	pp.pprint(request)
 	request_json = json.dumps(request)
-	handler = urllib2.urlopen(pqsUrl, request_json)
-	response_json = handler.read()
+	if secure:
+		opener = url.build_opener(u2k.HTTPKerberosAuthHandler())
+	else:
+		opener = url.build_opener()
+	opened = opener.open(pqsUrl, request_json)
+	response_json = opened.read()
 	response = json.loads(response_json)
 	print "Response : "
 	pp.pprint(response)
@@ -30,7 +48,7 @@ lengths = [ 8, 4, 4, 4, 12 ]
 connection_id = "-".join([random_string(x) for x in lengths])
 
 # Open a connection.
-print "Opening connection."
+print "Opening connection to {0}".format(pqsUrl)
 request = {
 	"request" : "openConnection",
 	"connectionId" : connection_id,
